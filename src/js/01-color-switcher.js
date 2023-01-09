c7045b4cdffa6c46d2b759118e0e4bd429f2a955
Kathryne-Kov <112563496+Kathryne-Kat@users.noreply.github.com>
2023-01-09 18:08:02 +0200

    add #1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyStyle= document.querySelector('body')
+let timer = null;
 
+startBtn.addEventListener("click", () => {
+    startBtn.setAttribute('disabled','')
+    timer = setInterval(() => {    
+        bodyStyle.style.backgroundColor = getRandomHexColor();    
+    }, 1000);
+});
+
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timer);
+    startBtn.removeAttribute('disabled')
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
