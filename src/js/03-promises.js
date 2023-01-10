cec9a5eb6c2d504c6568a312f0f9e0ae36661f27
Kathryne-Kov <112563496+Kathryne-Kat@users.noreply.github.com>
2023-01-11 00:52:38 +0200

    add #3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  formElem: document.querySelector('.form'),
+ };
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+        if (shouldResolve) {
+        // Fulfill
+          resolve({position, delay})
+        } else {
+        // Reject
+          reject({position, delay})
+      }
+    }, delay)  
+  }) 
+  //console.log(promise);
+  return promise;
 }
+
+refs.formElem.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log(e.target.elements);
+
+  let delay = Number(e.target.elements.delay.value);
+  const step = Number(e.target.elements.step.value);
+  const amount = Number(e.target.elements.amount.value);
+
+  console.log(delay, step, amount);
+  
+  for (let pos = 1; pos <= amount; pos += 1) {
+    createPromise(pos, delay)
+      .then(({position, delay}) => { Notiflix.Notify.success(`Fulfilled promise ${pos} in ${delay} ms`) })
+      .catch(({position, delay}) => { Notiflix.Notify.failure(`Reject promise ${pos} in ${delay} ms`) })
+      delay += step;
+    }  
+});
